26c373c29c8e76da5bd2fab7c072dea0f98916ea
nataliarosenberg <natalia.rosenberg87@gmail.com>
2023-04-24 23:18:18 +0200

    .

---
+++ 02_20.04.2023/Zadania Boole.py
@@ -0,0 +1,16 @@
+"""Czy dzielenie 29 przez 7 bez reszty wynosi 5?"""
+print(29 // 7 ==5)
+
+
+"""Wyświetl True, jeżeli liczba jest parzysta."""
+liczba = int(input("Podaj liczbę--> "))
+print("Rozwiązanie: ", liczba % 2 == 0)
+
+"""Czy 43 - 13 będzie się równać 11 + 12 ?"""
+print(43-13 == 11+12)
+
+"""Czy dzielenie 129 przez 17 bez reszty wynosi 3?"""
+print(129//17 ==3)
+
+"""Czy 247 podzielone przez 5 daje resztę 2?"""
+print(247%5 == 2)
