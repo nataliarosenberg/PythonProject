0c9a3298638fc2c561c1f24627e5c29031c5baa4
nataliarosenberg <natalia.rosenberg87@gmail.com>
2023-05-07 23:30:38 +0200

    .

---
+++ 03_27.04.2023/List - Zad.1.py
@@ -0,0 +1,12 @@
+"""1▹ Stwórz listę przedmiotów,
+które zabierzesz na samotną wyprawę w góry.
+ Elementy na liście posortuj alfabetycznie,
+a następnie wyświetl."""
+
+items = ["phone", "headphone", "water"]
+items.sort()
+print(items)
+
+for elem in items:
+    print(elem, "!")
+
---
+++ 03_27.04.2023/List - Zad.2.py
@@ -0,0 +1,17 @@
+"""
+Pobierz od użytkownika 10 liczb, wyświetl tylko te, które są nieparzyste.
+"""
+number= input("Enter 10 numbers, separate space -->  ")
+list_number=number.split()
+new_list=[]
+if len(list_number) == 10:
+    for num in list_number:
+        if int(num) % 2 >0:
+            print(num)
+            new_list.append(num)
+    print(new_list)
+else:
+    if len(list_number)>10:
+        print("You entered more numbers")
+    else:
+        print("You have entered to few numbers")
