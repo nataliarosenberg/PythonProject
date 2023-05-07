a3cf737784235b63ecd17c6d279e9c64485415f3
nataliarosenberg <natalia.rosenberg87@gmail.com>
2023-05-07 22:50:09 +0200

    .

---
+++ 03_25.04.2023/Zadanie 2 FOR.py
@@ -0,0 +1,12 @@
+"""
+Utwórz listę, która zawiera składniki na ulubione danie. Wyświetl komunikaty co należy pokolei dodać.
+Poza pętlą umieść pozostałe instrukcje np. “Wrzuć do pierkanika”, “Podawaj schłodzone”.
+"""
+ingredients = ["parmesan", "garlic", "basil","oil"]
+
+print("Do miski dodaj: ")
+
+for ingredients in ingredients:
+    print(ingredients)
+    print("then")
+print("Wszystko zblenduj i powstałe pesto połącz z makaronem ")
---
+++ 03_25.04.2023/Zadanie 4 FOR.py
@@ -0,0 +1,13 @@
+"""
+Napisz program, który wyświetli kolejne wyniki dla silni liczby naturalnej N
+(N podane przez użytkownika, ale nie większe niż 8).
+"""
+N = int(input("Enter number > 1 <= 8 --> "))
+
+if N <= 8 and N > 0:
+    silnia = 1
+    for i in range(2, N+1):
+        silnia = silnia*i
+    print("Silnia z ", i, "wynosi", silnia)
+else:
+    print("I can't calculate because your number is greater than 8")
